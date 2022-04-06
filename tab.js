c7e4fadb6f8b18344f941074209fee4cf1648a38
Lucas Araújo <herpetolucas@gmail.com>
2022-04-06 17:58:41 -0300

    usandoFOR

--- tab.js
+++ tab.js
@@ -7,21 +7,38 @@ function tabuada() {
         window.alert('Por favor, digite um número')
     } else {
         let n = Number(num.value)
-        let c = 1
+        
         tab.innerHTML = '' //esse comando limpa a tabuada velha antes de começar uma nova
-        while (c <= 10) {
+       /* 
+       PODE FAZER DE DOIS JEITOS, COM WHILE OU FOR
+       let c = 1
+       while (c <= 10) {
             let item = document.createElement('option')
             item.text = `${n} x ${c} = ${n*c}`
             tab.appendChild(item)
             c++
         }
-        let d = 11
+        */
+       for (let c = 1;c<=10;c++){
+        let item = document.createElement('option')
+        item.text = `${n} x ${c} = ${n*c}`
+        tab.appendChild(item)
+       }
+        
         tab2.innerHTML = ''
+        /*
+        let d = 11
         while (d <= 20) {
             let item2 = document.createElement('option')
             item2.text = `${n} x ${d} = ${n*d}`
             tab2.appendChild(item2)
             d++
         }
+        */
+       for (let d=11;d<=20;d++){
+            let item2 = document.createElement('option')
+            item2.text = `${n} x ${d} = ${n*d}`
+            tab2.appendChild(item2)
+       }
     }
 }
